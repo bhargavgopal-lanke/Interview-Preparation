dee9661b481beb6cb8435d7acd81d079496ac968
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2025-02-03 22:05:00 +0530

    Tabs Ui implemented in react.

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,41 @@
-
+import { useState } from "react";
+import Interests from "./Swiggy_interview_Quetion/Interests";
+import Profile from "./Swiggy_interview_Quetion/profile";
+import Settings from "./Swiggy_interview_Quetion/settings";
 import './App.css';
 
 function App() {
+  const [activeTabs, showActiveTabs] = useState(0);
+
+  const tabs = [
+    {
+      name: "profile",
+      component: Profile,
+    },
+    {
+      name: "settings",
+      component: Settings,
+    },
+    {
+      name: "interests",
+      component: Interests,
+    },
+  ];
+
+  const ActiveComponent = tabs[activeTabs].component;
+
   return (
     <div className="App">
-      
+      {tabs.map((t, index) => {
+        return (
+          <div key={index} className="tabs-sec">
+            <div onClick={() => showActiveTabs(index)}>{t.name}</div>
+          </div>
+        );
+      })}
+      <div className="tabs-sec-body">
+        <ActiveComponent />
+      </div>
     </div>
   );
 }
