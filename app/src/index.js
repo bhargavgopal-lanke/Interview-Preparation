a2de549eecd8ea116393dc2964e762e136ebc6d4
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2025-02-27 13:17:10 +0530

    Pagination practice.

--- app/src/index.js
+++ app/src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import './App.css';
+import Pagination from "./Pagination/Pagination";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Pagination />
   </React.StrictMode>
 );
 
