4737ec608c10b61227e4653c5a100a9275a95cc2
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2025-02-28 00:38:10 +0530

    pagination completed.

--- app/src/Pagination/Pagination.jsx
+++ app/src/Pagination/Pagination.jsx
@@ -3,10 +3,11 @@ import PaginationCard from "./PaginationCard";
 
 const Pagination = () => {
   const [products, setProducts] = useState({});
+  const [currentPage, setCurrentPage] = useState(0);
 
   const fetchProducts = async () => {
     try {
-      const data = await fetch("https://dummyjson.com/products");
+      const data = await fetch("https://dummyjson.com/products?limit=500");
       const json = await data.json();
 
       setProducts(json?.products);
@@ -15,16 +16,21 @@ const Pagination = () => {
     }
   };
 
-  // const MIN_PRODUCTS = 10;
-  // const totalProducts = products.length;
-  // const start = "";
-  // const end = "";
+  const PAGE_SIZE = 10;
+  const noOfPages = Math.ceil(products.length / PAGE_SIZE);
+  const start = currentPage * PAGE_SIZE;
+  const end = start + PAGE_SIZE;
 
   useEffect(() => {
     fetchProducts();
   }, []);
 
-  console.log([...Array(10).keys()]);
+  const handlePrev = () => {
+    setCurrentPage((prevState) => prevState - 1);
+  };
+  const handleNext = () => {
+    setCurrentPage((prevState) => prevState + 1);
+  };
 
   return (
     <div>
@@ -33,18 +39,39 @@ const Pagination = () => {
       ) : (
         <>
           <div className="pagination">
+            <button
+              className=""
+              onClick={handlePrev}
+              disabled={currentPage === 0}
+            >
+              ◀️
+            </button>
             {[...Array(10).keys()].map((n) => {
-              return <span className="num">{n}</span>;
+              return (
+                <span
+                  className={currentPage === n ? "num active" : "num"}
+                  onClick={() => setCurrentPage(n)}
+                >
+                  {n}
+                </span>
+              );
             })}
+            <button
+              onClick={handleNext}
+              disabled={currentPage === noOfPages - 1}
+            >
+              ▶️
+            </button>
           </div>
           <div className="flex">
-            {products?.slice(0, 10).map((x) => {
+            {products?.slice(start, end).map((x) => {
               return (
                 <PaginationCard
                   key={x.id}
                   title={x.title}
                   description={x.description}
                   thumbnail={x.thumbnail}
+                  noOfPages={noOfPages}
                 />
               );
             })}
