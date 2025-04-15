0c222e864786debd9a648c87a0fc7ea9f510fdb5
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2025-04-15 16:54:34 +0530

    created 5 input fields using array method. And with method looping 5 input fields

--- app/src/OTP_Verification/OtpVerfication.jsx
+++ app/src/OTP_Verification/OtpVerfication.jsx
@@ -1,47 +1,24 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useState } from "react";
 
 const OtpVerfication = () => {
-  const OTP_Fields = 5;
-
-  const [otp, setOtp] = useState(new Array(OTP_Fields).fill(""));
-
-  const inputRef = useRef([]);
-
-  console.log("ref", inputRef);
-
-  useEffect(() => {
-    inputRef.current[0]?.focus();
-  }, []);
+  const NO_OF_INPUTS = 5;
+  const [otp, setOtp] = useState(new Array(NO_OF_INPUTS).fill(""));
 
   const handleChange = (e, index) => {
-    let value = e.target.value;
-    if (isNaN(value)) return;
-    let newArr = [...otp];
-    newArr[index] = value.slice(-1);
-    setOtp(newArr);
-    if (value.length === 1) {
-      inputRef.current[index + 1]?.focus();
-    }
-  };
-
-  const handleChangeOnKeyDown = (e, index) => {
-    if(e?.target?.value === "" && e?.key === "Backspace") {
-        inputRef.current[index - 1]?.focus();
-    }
+    
   }
 
   return (
     <div>
-      {otp.map((x, index) => {
+      {otp.map((_, index) => {
         return (
-          <input
-            ref={(el) => (inputRef.current[index] = el)}
-            type="text"
-            key={index}
-            value={otp[index]}
-            onChange={(e) => handleChange(e, index)}
-            onKeyDown={(e) => handleChangeOnKeyDown(e, index)}
-          />
+          <>
+            <input
+              type="text"
+              value={otp[index]}
+              onChange={(e) => handleChange(e, index)}
+            />
+          </>
         );
       })}
     </div>
